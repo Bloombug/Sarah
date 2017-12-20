42ee89d1a1f2f26c74d21fd9e4c0642717c48880
Tomás Morais Pereira <1012641@sal.ipg.pt>
2017-12-20 16:26:45 +0000

    Initial commit

---
+++ app/src/main/java/bloom/sara/ProfileActivity.java
@@ -0,0 +1,21 @@
+package bloom.sara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    //Do all the work here BRO!!!!
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        setTitle("Profile");
+
+
+
+    }
+}
